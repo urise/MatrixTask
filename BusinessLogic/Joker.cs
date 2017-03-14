89511e1c6e172f5d65f5b8f89519c7d8a9b59513
Yuriy Dolzhykov <yuriy.d@sbtech.com>
2017-03-14 15:22:56 +0200

    Joker

---
+++ BusinessLogic/Joker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public static class Joker
+    {
+        private class Card
+        {
+            public int CardType { get; set; }
+            public int Count { get; set; }
+        }
+
+        private static bool IsOver(int jokersCount, List<Card> cards)
+        {
+            if (cards.Count == 0) return true;
+            if (cards.Count == 1) return cards[0].Count == 0 && jokersCount == 0;
+            if (cards[0].Count == 0 && (cards[1].Count == 0 || jokersCount == 0)) return true;
+            return false;
+        }
+
+        public static int GetNumberOfDecks(int jokersInitialCount, IEnumerable<int> cardsCounts)
+        {
+            var cardType = 1;
+            var cards = cardsCounts.Select(cnt => new Card {CardType = cardType++, Count = cnt}).ToList();
+            cards.Sort((card1, card2) => card1.Count.CompareTo(card2.Count));
+            int result = 0, jokersCount = jokersInitialCount;
+            while (!IsOver(jokersCount, cards))
+            {
+                result++;
+                if (cards.Count == 1)
+                {
+                    if (cards[0].Count == 0) jokersCount--;
+                    else cards[0].Count--;
+                }
+                else
+                {
+                    if (jokersCount > 0)
+                    {
+                        for (int i = 1; i < cards.Count; i++)
+                        {
+                            cards[i].Count--;
+                        }
+                        jokersCount--;
+                        if (cards[0].Count > cards[1].Count)
+                        {
+                            var card = cards[0];
+                            cards[0] = cards[1];
+                            cards[1] = card;
+                        }
+                    }
+                    else
+                    {
+                        foreach (var card in cards) card.Count--;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ MatrixTask/UnitTests/JokerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class JokerTests
+    {
+        [Test]
+        public void GetNumberOfDecksTest1()
+        {
+            Assert.That(Joker.GetNumberOfDecks(3, new[] {10, 15}), Is.EqualTo(13));
+        }
+
+        [Test]
+        public void GetNumberOfDecksTest2()
+        {
+            Assert.That(Joker.GetNumberOfDecks(4, new[] { 1, 2, 3 }), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void GetNumberOfDecksTest3()
+        {
+            Assert.That(Joker.GetNumberOfDecks(5, new[] { 1 }), Is.EqualTo(6));
+        }
+
+        [Test]
+        public void GetNumberOfDecksTest4()
+        {
+            Assert.That(Joker.GetNumberOfDecks(3, new[] { 2, 3, 4, 5, 6 }), Is.EqualTo(4));
+        }
+
+    }
+}
